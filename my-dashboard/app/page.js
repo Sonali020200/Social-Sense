a81a81bb4e8e648d09fd2e27ea91f75e06996016
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-27 10:07:17 +0530

    final commit

--- my-dashboard/app/page.js
+++ my-dashboard/app/page.js
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="w-full h-screen flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold mb-8">Welcome to Social Sense Assignment!</h1>
-        
-      </div>
+      <div className="w-full h-screen flex flex-col justify-center items-center text-center">
+  <h1 className="text-4xl font-bold mb-8">Welcome to Social Sense Assignment!</h1>
+</div>
+
     </>
   );
 }
